2655157b8652754304bf34e31613a9859d030bd6
Anthony Clark Perfecto <a1clark1a@gmail.com>
2020-02-26 15:31:50 -0800

    added user info to the comment request by id

--- src/comments-router/comments-service.js
+++ src/comments-router/comments-service.js
@@ -13,7 +13,25 @@ const CommentsService = {
   getCommentsById(knex, commentsId) {
     return knex
       .from("memes_comments AS com")
-      .select("*")
+      .select(
+        "com.id",
+        "com.comment",
+        "com.memes_id",
+        "com.user_id",
+        "com.date_created",
+        knex.raw(
+          `row_to_json(
+            (SELECT tmp FROM (
+              SELECT
+              usr.id,
+              usr.user_name,
+              usr.full_name,
+              usr.date_created
+            ) tmp)
+          ) AS "user"`
+        )
+      )
+      .leftJoin("memes_users As usr", "com.user_id", "usr.id")
       .where("com.id", commentsId)
       .first();
   },
